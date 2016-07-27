3051b1fd897a9159fc3cd30f37f8008b50814092
Luca Melis <lucamelis12@gmail.com>
2016-07-27 18:07:22 +0100

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'ncco_record_actions#index', as: 'record'
+
   resources :ncco_record_actions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
