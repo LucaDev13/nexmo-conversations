438b2ac1c84cdc498d6c11040aeaf372bd001382
Luca Melis <lucamelis12@gmail.com>
2016-12-07 14:46:59 +0000

    allow json webhooks- POST calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
-  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+  skip_before_action :verify_authenticity_token, if: :json_request?
   before_action :authenticate_admin!
   protect_from_forgery with: :null_session
 
-
+  protected
+  def json_request?
+    request.format.json?
+  end
 end
