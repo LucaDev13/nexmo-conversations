3270dde9f06badbb32f92dfd3b811321f6ea17cd
Luca Melis <lucamelis12@gmail.com>
2016-12-07 14:50:03 +0000

    revert controller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token, if: :json_request?
   before_action :authenticate_admin!
   protect_from_forgery with: :null_session
 
-  protected
-  def json_request?
-    request.format.json?
-  end
+
 end
